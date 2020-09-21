f0a5ef41affb976ba1da92757b1ab51170571c18
David Lai <davidlatwe@gmail.com>
2020-09-21 22:04:21 +0800

    decouple

---
+++ 1.1.0/package.py
@@ -0,0 +1,26 @@
+
+name = "opencolorio"
+
+version = "1.1.0"
+
+variants = [
+    ["arch-*", "os-*", "release-1"],
+    ["arch-*", "os-*", "release-0"],
+]
+
+build_requires = [
+    "rezutil-1+",
+    "cmake-3.2+",
+]
+build_command = "python {root}/rezbuild.py {install}"
+
+
+def commands():
+    env = globals()["env"]
+
+    env.PATH.append("{root}/bin")
+    env.PATH.append("{root}/lib")
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
+
+    env.OCIO_LOCATION = "{root}"
